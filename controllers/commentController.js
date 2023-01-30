74a3ae822fb8d605375d3c5607bb062f8a5ab02b
Ehsan Shadmehri <ehsanshadmehri@gmail.com>
2023-01-30 16:24:07 +0330

    add change comment vote fn in

--- controllers/commentController.js
+++ controllers/commentController.js
@@ -1,5 +1,6 @@
 const { Comments } = require('../model');
 const { User } = require('../model');
+const { Vote } = require('../model');
 const { validationResult } = require('express-validator');
 module.exports = {
     async addComment(req, res) {
@@ -25,7 +26,7 @@ module.exports = {
     },
     async getComments(req, res) {
         let comments = await Comments.findAll({
-            include: [{ model: User, attributes: ['name'] }],
+            include: [{ model: User, attributes: ['name', 'id'] }],
         }).catch(e => {
             return res.json({ msg: 'An error occured' });
         });
@@ -46,6 +47,43 @@ module.exports = {
         await Comments.update({ text }, { where: { id } }).catch(e => {
             res.status(401);
         });
+        res.status(200).send({});
+    },
+    async changeVote(req, res) {
+        const { userId: user_id, commentId: comment_id, vote } = req.body;
+        const voteBefore = await Vote.findOne({
+            where: { user_id, comment_id },
+        });
+        if (voteBefore) {
+            if (voteBefore.vote !== vote) {
+                await Comments.increment('votes', {
+                    by: vote,
+                    where: { id: comment_id },
+                }).catch(e => {
+                    res.status(401);
+                });
+                await Vote.increment('vote', {
+                    by: vote,
+                    where: { id: voteBefore.id },
+                }).catch(e => {
+                    res.status(401);
+                });
+            }
+        } else {
+            await Vote.create({
+                user_id,
+                comment_id,
+                vote,
+            }).catch(e => {
+                res.status(401);
+            });
+            await Comments.increment('votes', {
+                by: vote,
+                where: { id: comment_id },
+            }).catch(e => {
+                res.status(401);
+            });
+        }
         res.status(200).send({});
     },
 };
--- model/index.js
+++ model/index.js
@@ -11,7 +11,7 @@ const sequelize = new Sequelize(
 );
 // Bookmark and History depend on Song, and User
 // so import it last
-['User.js', 'Comments'].forEach(file => {
+['User.js', 'Comments', 'Vote'].forEach(file => {
     const filename = path.join(__dirname, file);
     const model = require(filename)(sequelize, Sequelize.DataTypes);
     db[model.name] = model;
@@ -28,5 +28,11 @@ db.Sequelize = Sequelize;
 
 db['Comments'].belongsTo(db['User'], { foreignKey: 'user_id' });
 db['User'].hasMany(db['Comments'], { foreignKey: 'user_id' });
+
+db['Vote'].belongsTo(db['User'], { foreignKey: 'user_id' });
+db['User'].hasMany(db['Vote'], { foreignKey: 'user_id' });
+
+db['Vote'].belongsTo(db['Comments'], { foreignKey: 'comment_id' });
+db['Comments'].hasMany(db['Vote'], { foreignKey: 'comment_id' });
 
 module.exports = db;
